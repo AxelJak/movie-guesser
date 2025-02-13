d9df38b66556f27415478625d5ca2060af417aba
Axel Jakobsson <axel.jakobsson3@gmail.com>
2025-02-13 13:13:06 +0100

    motion label added and rough animation + logic

--- src/components/room/RoomInput.tsx
+++ src/components/room/RoomInput.tsx
@@ -3,12 +3,10 @@ import { useEffect, useState } from "react";
 import { useQuery } from "@rocicorp/zero/react";
 import { createMessage } from "@/utils/message";
 import { createGuess } from "@/utils/guess";
-import { Check } from "lucide-react"
-
-import { cn } from "@/lib/utils"
 import { Input } from "@/components/ui/input"
 import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "@/components/ui/command"
 import { Movie } from "@/schema";
+import { motion, AnimatePresence } from "motion/react";
 
 export type AutocompleteOption = {
   value: string
@@ -18,8 +16,7 @@ export default function RoomInput({ listId, roomId, playerId }: { listId: string
   const z = useZero();
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState("")
-  const [message, setMessage] = useState('');
-  const [guess, setGuess] = useState('');
+  const [chat, setChat] = useState(false);
   const listQuery = z.query.list.related('movies').where('id', listId)
   const [list] = useQuery(listQuery.one());
   const [movies, setMovies] = useState<Movie[]>([])
@@ -31,56 +28,86 @@ export default function RoomInput({ listId, roomId, playerId }: { listId: string
   }, [list])
 
   function sendMessage() {
-    if (!message) return;
     z.mutate.message.insert(
-      createMessage(message, playerId, roomId)
+      createMessage(value, playerId, roomId)
     );
   }
 
   function sendGuess() {
-    if (!guess) return;
     z.mutate.guess.insert(
-      createGuess(guess, playerId, roomId)
+      createGuess(value, playerId, roomId)
     );
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value
     setValue(inputValue)
+    if(!chat) return;
     setOpen(inputValue.length > 0)
-
     const filtered = movies.filter((movie) => movie.title.toLowerCase().includes(inputValue.toLowerCase()))
     setFilteredMovies(filtered)
   }
 
-  const handleSelect = (selectedValue: string) => {
-    console.log(selectedValue);
-    sendGuess()
+  const handleSelect = () => {
+    if (!value) return;
+    if (chat) {
+      sendMessage()
+    } else {
+      sendGuess()
+      setOpen(false);    
+    }
     setValue("")
-    setOpen(false);    
   }
 
   function handleKeyDownEvent(event: any) {
     if (event.key === 'Enter') {
-      handleSelect(filteredMovies[0].title);
+      if(!chat) {
+        setValue(filteredMovies[0].title);
+      }
+      handleSelect();
+    }
+    if (event.ctrlKey && event.key === 's'){
+      setChat(!chat);
     }
   }
 
   return (
     <div className="relative">
+      <AnimatePresence mode="wait">
+        { chat ? 
+          <motion.label 
+            key="message"
+            className="text-xs bg-green-200 p-1 rounded-t-sm"
+            initial={{ opacity: 0.5, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0.5, scale: 0.5 }}
+          >
+            Message
+          </motion.label> :
+          <motion.label
+            key="guess"
+            className="text-xs bg-blue-400 p-1 rounded-t-sm"
+            initial={{ opacity: 0.5, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0.5, scale: 0.5 }}
+          >
+            Guess movie
+          </motion.label>
+        }
+      </AnimatePresence>
       <Input
         type="text"
+        className={chat ? "border-green-200 rounded-tl-none" : "border-blue-400 rounded-tl-none"} 
         placeholder={""}
         value={value}
         onChange={handleInputChange}
         onKeyDown={handleKeyDownEvent}
-        className=""
       />
-      {open && (
+      {open && !chat && (
         <div className="absolute w-full mt-1 bg-background border rounded-md shadow-md z-10">
           <Command>
             <CommandList>
-              <CommandEmpty>{"No movies in list"}</CommandEmpty>
+              <CommandEmpty>{"No results found"}</CommandEmpty>
               <CommandGroup>
                 {filteredMovies.map((movie) => (
                   <CommandItem key={movie.id} value={movie.title} onSelect={handleSelect}>
